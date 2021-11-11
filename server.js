4ed491abb984ac6fa7901db4652903f600bdd2e2
Roald Välja <roaldvlja@gmail.com>
2021-11-11 17:19:49 +0200

    added css

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    let url = `https://api.spoonacular.com/recipes/random/?apiKey=${process.env.APIKEY}`;
+    axios.get(url)
+    .then((response) => {
+        let recipe = {
+            title: response.data.recipes[0].title,
+            ingredients: response.data.recipes[0].extendedIngredients,
+            image: response.data.recipes[0].image,
+            summary: response.data.recipes[0].summary,
+            instructions: response.data.recipes[0].instructions
+        }
+        res.render('index.ejs', {dataFromSpoonacular: recipe});
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+});
 
 
 
